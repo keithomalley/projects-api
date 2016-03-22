089728f80379a885349cb190a1d00b90ed754b30
Keith O'Malley <keithcharlieom@gmail.com>
2016-03-22 18:19:45 +0000

    removed nodemodules

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require('express'),
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
 app.configure(function (){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -14,5 +17,6 @@ app.post('/projects', projects.addProject);
 app.put('/projects/:id', projects.updateProject);
 app.delete('/projects/:id');
 
-app.listen(3000);
-console.log("Listening on port 3000...");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
